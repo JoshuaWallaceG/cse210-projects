88866ec5f19a2137f5b49283a0ed3369cb0bab1d
Joshua Wallace <joshuawallaceg@gmail.com>
2025-09-20 13:17:02 -0600

    Added letter grade determiner

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+        char letter;
+
+        if (grade >= 90)
+        {
+            letter = 'A';
+        }
+        else if (grade >= 80)
+        {
+            letter = 'B';
+        }
+        else if (grade >= 70)
+        {
+            letter = 'C';
+        }
+        else if (grade >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed the class! Congrats!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class. :( I know you can do better next time!");
+        }
     }
 }
