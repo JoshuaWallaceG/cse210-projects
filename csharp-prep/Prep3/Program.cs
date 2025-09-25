24c940574f47a9aac0cef2424d145698f74bd4f2
Joshua Wallace <joshuawallaceg@gmail.com>
2025-09-25 11:36:59 -0600

    Added the Magic Number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 25);
+        int userGuess;
+
+        do
+        {
+            Console.Write("Guess the magic number: ");
+            userGuess = int.Parse(Console.ReadLine());
+            if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (userGuess == magicNumber)
+            {
+                Console.WriteLine($"You got it! The number was {magicNumber}");
+            }
+        } while (userGuess != magicNumber);
     }
 }
