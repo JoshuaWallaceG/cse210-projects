189617291ea5bc2c4ab4da279ffec5b0680616cb
Joshua Wallace <joshuawallaceg@gmail.com>
2025-10-01 18:14:43 -0600

    Learned about classes

---
+++ sandbox/Sandbox/InClassSandbox copy.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        //Prevent crashing!
+        // string userInput = Console.ReadLine();
+        // int.TryParse(userInput, out userNumber);
+
+
+
+
+
+    }
+}
