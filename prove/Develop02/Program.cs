a8ecc8b42657b2b66ada7e0bd7156d899d66de84
Joshua Wallace <joshuawallaceg@gmail.com>
2025-10-11 23:51:20 -0600

    Added creatitivy comment

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -3,7 +3,17 @@ using System.IO.Compression;
 
 class Program
 {
-    static void Main(string[] args)
+
+/*
+I demonstrated creativity by adding a small part of mindfulness to the program. 
+One of the reasons why I didn't journal for a long time was that I was only journaling informationally, in the sense that I was just documenting what happened in the day. 
+Because of that, I was never getting anything out of it other than a log book of my daily life.
+When I started journaling and adding in things like how I felt and logging my emotions, I was able to feel more mindful of my current state of mind and identify the different events that caused me to feel one way or the other. 
+
+As such, I've decided to add a simple question: "In 1 word, what is your mood right now?"
+As a moment is taken to reflect, and then recorded, we are able to become more and more aware of not only what we did, but how we felt as well.    
+*/
+static void Main(string[] args)
     {
         Journal currentJournal = new Journal();
         int userChoice;
@@ -27,7 +37,7 @@ class Program
                 case 4:
                     currentJournal.SaveToFile();
                     break;
-                //No need for default switch case because GetMenuChoice will never return a value outside of 1-4
+                    //No need for default switch case because GetMenuChoice will never return a value outside of 1-4
             }
         } while (userChoice != 0);
         Console.WriteLine("Goodbye!");
