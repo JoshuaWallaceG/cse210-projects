774942ca4cafc758268270b5b7db5a9bc67c8908
Joshua Wallace <joshuawallaceg@gmail.com>
2025-10-24 13:03:42 -0600

    Added creativity comment

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,6 +2,13 @@ using System;
 
 class Program
 {
+
+    /*
+    I demonstrated creativity by adding a "blank percentage" prompt and variable that allows the user to customize what percentage of the prompt they would like to hide each round, effectively adding a customizable difficulty option for the user. 
+    To make sure that the user did not input a value like 0, which would cause the program to clear nothing each round, I also added a input validation check to only accept percentage values from 1-100. 
+    Because the user could input low values like 1% and 2%, I also added code to assure that at least one word was blanked each round instead of the possibility of no words being blanked when using said lower percentages.
+    */
+
     static void Main(string[] args)
     {
 
@@ -35,8 +42,7 @@ class Program
             {
                 validBlankNum = true;
             }
-        }
-        while (!validBlankNum);
+        } while (!validBlankNum);
 
         //In order to both begin with no blanks AND end with it fully blanked, I have to have atleast 1 of the  prints/clear screens outside the loop.
         Console.Clear();
