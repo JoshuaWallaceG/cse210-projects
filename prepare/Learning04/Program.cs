d554c7f0d8b5c4437522fde5f277b57b647db7fd
Joshua Wallace <joshuawallaceg@gmail.com>
2025-11-01 17:19:56 -0600

    Added assignment program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        
+        string n1 = "Ben Board";
+        string t1 = "Science";
+        
+        string n2 = "James John";
+        string t2 = "Maths";
+        string c2 = "Section 3";
+        string p2 = "6.2 - 6.7";
+
+        string n3 = "Mary Wander";
+        string t3 = "Writing";
+        string b3 = "How To Fly";
+
+        Assignment myAssignment = new Assignment(n1, t1);
+        MathAssignment myMathAssignment = new MathAssignment(n2, t2, c2, p2);
+        WritingAssignment myWritingAssignment = new WritingAssignment(n3, t3, b3);
+        
+        Console.WriteLine(myAssignment.GetSummary());
+        Console.WriteLine();
+        Console.WriteLine(myMathAssignment.GetSummary());
+        Console.WriteLine(myMathAssignment.GetHomeworkList());
+        Console.WriteLine();
+        Console.WriteLine(myWritingAssignment.GetSummary());
+        Console.WriteLine(myWritingAssignment.GetWritingInformation());
     }
 }
