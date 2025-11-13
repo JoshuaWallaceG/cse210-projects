eea102255fdcbfb5f1f9eb780378c70e0597628a
Joshua Wallace <joshuawallaceg@gmail.com>
2025-11-13 10:24:33 -0700

    Added shape area/color program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square s1 = new Square("Red", 5);
+        Rectangle r1 = new Rectangle("Green", 10, 2);
+        Circle c1 = new Circle("Purple", 3);
+
+        Console.WriteLine($"The color of the square is {s1.GetColor()} and the area is {s1.GetArea()}");
+        Console.WriteLine($"The color of the rectangle is {r1.GetColor()} and the area is {r1.GetArea()}");
+        Console.WriteLine($"The color of the circle is {c1.GetColor()} and the area is {c1.GetArea()}");
     }
 }
